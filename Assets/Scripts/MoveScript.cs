de5af68068f29128059aa2803a5073237526cd23
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-12-08 02:52:54 -0500

    Added the Enemy prefab with a MoveScript, which just makes the Enemy move off to the left.

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	// Editor attributes
+	public Vector2 speed = new Vector2(5,5);
+	public Vector2 direction = -Vector2.up;
+
+	// Update is called once per frame
+	void Update () {
+
+		float velocityX = speed.x * direction.x;
+		float velocityY = speed.y * direction.y;
+
+		Vector3 movement = new Vector3(velocityX, velocityY, 0);
+
+		movement *= Time.deltaTime;
+		this.transform.Translate(movement);
+	}
+}
