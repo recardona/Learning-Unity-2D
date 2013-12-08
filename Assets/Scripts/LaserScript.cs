edbe33880f0cb96369bf25fb735764c9d5638317
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-12-08 03:25:54 -0500

    Added RedLaser that just travels forward.

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour {
+
+	// Editor attributes
+	public int damage = 1;
+	public bool isEnemyShot = false;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 20); //20 second time-to-live
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
