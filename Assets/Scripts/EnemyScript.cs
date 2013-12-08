7312590420c949334485145f22ff388012b68610
Rogelio E. Cardona-Rivera <mail@rogel.io>
2013-12-08 17:49:08 -0500

    Added a FireLaserScript and an EnemyAI Controller Script to the Enemy Prefab.  The Enemy now moves to the left and shoots lasers at a cool down rate of 1 second.

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Generic Enemy Behavior
+/// </summary>
+public class EnemyScript : MonoBehaviour {
+
+	private FireLaserScript laser;
+
+	// Awake is called when the script is loaded
+	private void Awake () {
+		//retrieve the weapon!
+		laser = GetComponent<FireLaserScript>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Auto-fire
+		if (laser != null && laser.CanShoot) {
+			laser.Shoot(true); //it's true, this is an enemy shooting
+		}
+	}
+}
